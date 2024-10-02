42613ce89ddc59ef988422378e6ae3830814db18
Samuel Molendolff Teixeira <contact@samuelmteixeira.dev>
2024-10-01 21:15:04 -0300

    ci: add pipelines

--- variables.tf
+++ variables.tf
@@ -1,16 +1,51 @@
-variable "prefix" {}
-variable "cluster_name" {}
-variable "retentions_days" {}
-variable "desired_size" {}
-variable "max_size" {}
-variable "min_size" {}
+variable "prefix" {
+  description = "Prefix for resources"
+  type        = string
+  default = "fiap-01"
+}
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "fiap-eks-cluster"
+}
+variable "retentions_days" {
+  description = "Number of days to retain logs"
+  type        = number
+  default     = 7
+}
+variable "desired_size" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
+}
+variable "max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 3
+}
+variable "min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+  default     = 1
+}
 variable "db_password" {
   description = "Password for RDS"
   type        = string
   sensitive   = true
+  default = "postgres"
 }
-variable "db_name" {}
+variable "db_name" {
+  description = "Name of the RDS database"
+  type        = string
+  default = "restaurant"
+}
 
-variable "db_user" {}
-
-variable "kubeconfig" {}
+variable "db_user" {
+  description = "Username for RDS"
+  type        = string
+  default = "postgres"
+}
+variable "kubeconfig" {
+  description = "Kubeconfig file"
+  type        = string
+}
